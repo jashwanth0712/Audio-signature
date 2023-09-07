495a2efe3ec9c7d5979a3a9b3b0d185772678ca8
jashwanth0712 <jashwanth0712@gmail.com>
2023-09-07 23:11:11 +0530

    added regenerate

--- temporary/src/components/audio_recorder.jsx
+++ temporary/src/components/audio_recorder.jsx
@@ -2,7 +2,7 @@ import { useState, useRef } from "react";
 
 
 const AudioRecorder = () => {
-    const mimeType = "audio/webm";
+    const mimeType = "audio/mp3";
 
     const [permission, setPermission] = useState(false);
     const [stream, setStream] = useState(null);
@@ -10,7 +10,7 @@ const AudioRecorder = () => {
     const [recordingStatus, setRecordingStatus] = useState("inactive");
     const [audioChunks, setAudioChunks] = useState([]);
     const [audio, setAudio] = useState(null);
-
+    const [positiveIntArray, setPositiveIntArray] = useState([]);
     const getMicrophonePermission = async () => {
         if ("MediaRecorder" in window) {
             try {
@@ -45,22 +45,72 @@ const AudioRecorder = () => {
         };
         setAudioChunks(localAudioChunks);
     };
+    function binaryDataToPositiveIntArray(binaryData) {
+        const uintArray = new Uint8Array(binaryData);
+        const positiveIntArray = Array.from(uintArray);
+        return positiveIntArray;
+    }
+    const regenerateAudio = () => {
+        if (positiveIntArray.length === 0) {
+            console.error("No positive integer array to regenerate audio from.");
+            return;
+        }
 
+        // Create an AudioContext
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+        // Create an empty audio buffer
+        const audioBuffer = audioContext.createBuffer(1, positiveIntArray.length, audioContext.sampleRate);
+
+        // Copy the positive integer array into the audio buffer
+        const audioData = audioBuffer.getChannelData(0);
+        for (let i = 0; i < positiveIntArray.length; i++) {
+            audioData[i] = positiveIntArray[i] / 255; // Normalize to the range [0, 1]
+        }
+
+        // Create an audio buffer source and play it
+        const source = audioContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(audioContext.destination);
+        source.start();
+    };
     const stopRecording = () => {
         setRecordingStatus("inactive");
-        //stops the recording instance
+        // Stops the recording instance
         mediaRecorder.current.stop();
         mediaRecorder.current.onstop = () => {
-            //creates a blob file from the audiochunks data
+            // Creates a blob file from the audioChunks data
             const audioBlob = new Blob(audioChunks, { type: mimeType });
-            //creates a playable URL from the blob file.
+    
+            // Create a new FileReader
+            const reader = new FileReader();
+    
+            // Define a callback function to handle the FileReader onload event
+            reader.onload = function(event) {
+                // The result property of the FileReader contains the binary representation
+                const binaryData = event.target.result;
+    
+                // Convert binary data to an array of positive integers
+                setPositiveIntArray( binaryDataToPositiveIntArray(binaryData))
+    
+                // Now, you have the audio data as a single-dimensional array of positive integers
+                console.log("Positive Integer Array:", positiveIntArray);
+    
+                // Optionally, you can create audio from this array using the function you mentioned.
+                // Example: createAudioFromPositiveIntArray(positiveIntArray);
+            };
+    
+            // Read the audioBlob as an ArrayBuffer
+            reader.readAsArrayBuffer(audioBlob);
+    
+            // Optionally, you can also create a playable URL from the blob file as before
             const audioUrl = URL.createObjectURL(audioBlob);
             setAudio(audioUrl);
             setAudioChunks([]);
-            console.log("stop ran")
+            console.log("stop ran");
         };
     };
-
+    
     return (
         <div>
             <h2>Audio Recorder</h2>
@@ -89,9 +139,15 @@ const AudioRecorder = () => {
                             Stop Recording
                         </button>
                     ) : null}
+                    {positiveIntArray.length > 0 ? (
+                        <button onClick={regenerateAudio} type="button">
+                            Regenerate Audio
+                        </button>
+                    ) : null}
                 </div>
             </main>
         </div>
     );
 };
-export default AudioRecorder
+
+export default AudioRecorder;
