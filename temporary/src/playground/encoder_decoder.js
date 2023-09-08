994feee94014433d813c2698ffd4e84ef4efb16c
jashwanth0712 <jashwanth0712@gmail.com>
2023-09-08 10:31:43 +0530

    updated the encoder to store data in variable

--- temporary/src/playground/encoder_decoder.js
+++ temporary/src/playground/encoder_decoder.js
@@ -3,7 +3,6 @@ import path from 'path';
 
 // Define file paths
 const mp3FilePath = 'sample.mp3';
-const encodedTextFilePath = 'encodedmp3.txt';
 const decodedMP3Directory = 'decoded_mp3';
 const decodedMP3FilePath = path.join(decodedMP3Directory, 'decoded_mp3_file.mp3');
 
@@ -12,24 +11,24 @@ if (!fs.existsSync(decodedMP3Directory)) {
   fs.mkdirSync(decodedMP3Directory);
 }
 
-// Encode MP3 to text
-function encodeMP3ToText(mp3FilePath, textFilePath) {
+// Encode MP3 to text and store it as a string
+function encodeMP3ToText(mp3FilePath) {
   const mp3Data = fs.readFileSync(mp3FilePath);
   const encodedText = Buffer.from(mp3Data).toString('base64');
-  fs.writeFileSync(textFilePath, encodedText);
   console.log('MP3 encoded as text.');
+  return encodedText; // Return the encoded text
 }
 
 // Decode text back to MP3
-function decodeTextToMP3(textFilePath, outputMP3FilePath) {
-  const encodedText = fs.readFileSync(textFilePath, 'utf-8');
+function decodeTextToMP3(encodedText, outputMP3FilePath) {
   const mp3Data = Buffer.from(encodedText, 'base64');
   fs.writeFileSync(outputMP3FilePath, mp3Data);
   console.log('Text decoded to MP3.');
 }
 
-// Encode MP3 to text and save as encodedmp3.txt
-encodeMP3ToText(mp3FilePath, encodedTextFilePath);
+// Encode MP3 to text and save it as a variable
+const encodedMP3Text = encodeMP3ToText(mp3FilePath);
+console.log(encodedMP3Text)
 
 // Decode text back to MP3
-decodeTextToMP3(encodedTextFilePath, decodedMP3FilePath);
+decodeTextToMP3(encodedMP3Text, decodedMP3FilePath);
