d57130b623229c23951432b56d11292e34d3a965
jashwanth0712 <jashwanth0712@gmail.com>
2023-09-23 15:01:50 +0530

    updated styling

--- extension/public/background.js
+++ extension/public/background.js
@@ -19,7 +19,7 @@ chrome.commands.onCommand.addListener((command) => {
         target: { tabId: activeTab.id },
         function: () => {
         
-          const body = document.getElementById('root');
+          const body = document.getElementById('maestro-portal');
           const currentFilter = body.style.filter;
           if (currentFilter.includes('blur')) {
             // Remove the blur filter
@@ -32,13 +32,27 @@ chrome.commands.onCommand.addListener((command) => {
               // If it doesn't exist, create one and append it to the body
               const newInputField = document.createElement('input');
               newInputField.id = 'yourInputFieldId';
-              newInputField.placeholder = 'hello';
+              newInputField.placeholder = 'Ask AI ';
+              
               // Apply CSS styles to position it absolutely at the top center
               newInputField.style.position = 'absolute';
-              newInputField.style.top = '0';
+              newInputField.style.top = '40%';
               newInputField.style.left = '50%';
+              newInputField.style.border = 'none';
+              
               newInputField.style.transform = 'translateX(-50%)';
+              // Apply additional CSS styles
+              newInputField.style.boxShadow = '0 8px 30px rgba(0, 0, 0, 0.3)';
+              newInputField.style.padding = '10px';
+              newInputField.style.color = 'black';
+              newInputField.style.width = '40%';
+              newInputField.style.backgroundColor = 'rgba(211, 211, 211, 0.1)';
+              newInputField.style.borderRadius = '10px';
+              newInputField.style.fontSize = 'x-large';
+              newInputField.style.zIndex='100';
+              
               document.body.appendChild(newInputField);
+              
             }
             body.style.filter = 'blur(10px)';
           }
