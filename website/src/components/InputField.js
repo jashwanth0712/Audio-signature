cb4ccf02e12bbeaedf75ed1b2f280dd7acc2c14a
jashwanth0712 <jashwanth0712@gmail.com>
2023-10-07 23:22:40 +0530

    added input field

--- website/src/components/InputField.js
+++ website/src/components/InputField.js
@@ -1,25 +1,44 @@
 // inputField.js
-import "../App.css"
-
+import React, { useState } from "react";
+import "../App.css";
+import sendIcon from "../assets/icons/arrow.png"
 export default function InputField() {
-    const body = document.getElementById('root');
-    const inputField = document.getElementById('yourInputFieldId');
-  
-    if (!inputField) {
-      const newInputField = document.createElement('input');
-      newInputField.id = 'yourInputFieldId';
-      newInputField.placeholder = 'Ask AI';
-      newInputField.setAttribute('type', 'text');
-  
-      body.appendChild(newInputField);
-    }
-  
-    body.style.filter = 'blur(10px)';
-  
-    // You can define and reference 'listeningAnimation' here if needed.
-    // listeningAnimation.style.display = 'block';
-  
-    inputField.style.display = 'block';
-    inputField.focus();
-  }
-  
+  const [inputValue, setInputValue] = useState(""); // State to track input value
+
+  // Function to handle input value change
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform your submit action here with the inputValue
+    console.log("Submitted: " + inputValue);
+  };
+
+  // Calculate the number of rows based on the number of newline characters
+  const calculateRowCount = () => {
+    return inputValue.split("\n").length;
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        className="inputfield"
+        value={inputValue}
+        onChange={handleInputChange}
+        rows={calculateRowCount()}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && e.shiftKey === false) {
+            e.preventDefault(); // Prevent default behavior of Enter (submitting the form)
+            handleSubmit(e); // Call the submit function
+          }
+        }}
+      />
+      <button type="submit" className="submit-button">
+        <img src={sendIcon}  />
+      </button>
+    </form>
+  );
+}
--- website/src/pages/prompt.js
+++ website/src/pages/prompt.js
@@ -2,7 +2,7 @@ import InputField from "../components/InputField"
 import ListeningAnimation from "../components/listening"
 export default prompt=()=>{
     return(
-        <div>
+        <div className="App">
             <ListeningAnimation/>
             <InputField/>
         </div>
