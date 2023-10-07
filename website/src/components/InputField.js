d186f77a2ab228ba9c9329fa5e3801c6d589a31a
aasri0907 <asrigopalam@gmail.com>
2023-10-07 22:39:35 +0530

    added inputfield

--- website/src/components/InputField.js
+++ website/src/components/InputField.js
@@ -1,5 +1,25 @@
-export default function InputField(){
-    return (
-        <h2>input</h2>
-    )
-}
+// inputField.js
+import "../App.css"
+
+export default function InputField() {
+    const body = document.getElementById('root');
+    const inputField = document.getElementById('yourInputFieldId');
+  
+    if (!inputField) {
+      const newInputField = document.createElement('input');
+      newInputField.id = 'yourInputFieldId';
+      newInputField.placeholder = 'Ask AI';
+      newInputField.setAttribute('type', 'text');
+  
+      body.appendChild(newInputField);
+    }
+  
+    body.style.filter = 'blur(10px)';
+  
+    // You can define and reference 'listeningAnimation' here if needed.
+    // listeningAnimation.style.display = 'block';
+  
+    inputField.style.display = 'block';
+    inputField.focus();
+  }
+  
